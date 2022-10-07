0220e6c1da9c495920ec901e92ef0d01e09d44ee
Project_TL <me@projecttl.net>
2022-10-07 15:06:32 +0900

    feat: middle save #1

--- emoji/cube.go
+++ emoji/cube.go
@@ -5,6 +5,27 @@ package emoji
  *  @ServerURL https://discord.gg/vgvYdUdnss
  */
 const (
-	CubeChecking  Emoji = "<a:checking:923822230255845396>"
-	CubeNoMarking Emoji = "<a:no_marking:923823041564278805>"
+	// Animation Emoji
+	CubeAlert      Emoji = "<a:alert:952180698486288415>"
+	CubeBongo      Emoji = "<a:bongo:976774020726915082>"
+	CubeBongoRage  Emoji = "<a:bongo_rage:976774136552632320>"
+	CubeBongoHyper Emoji = "<a:bongo_hyper:976774078054662100>"
+	CubeChecking   Emoji = "<a:checking:923822230255845396>"
+	CubeDancing    Emoji = "<a:dancing:997499912122007672>"
+	CubeMinecraft  Emoji = "<a:minecraft:932939103358582815>"
+	CubeNoMarking  Emoji = "<a:no_marking:923823041564278805>"
+
+	// Non-Animation Emoji
+	CubeGit        Emoji = "<:git:935741709273403413>"
+	CubeGoland     Emoji = "<:goland:935740790989279232>"
+	CubeGolang     Emoji = "<:golang:935737078405287956>"
+	CubeIntelliJ   Emoji = "<:intellij:935740682168066078>"
+	CubeJavaScript Emoji = "<:javascript:935737191349493830>"
+	CubeKotlin     Emoji = "<:kotlin:935737227458273290>"
+	CubeMySQL      Emoji = "<:mysql:935737316608208896>"
+	CubeNodeJS     Emoji = "<:nodejs:935737383490572308>"
+	CubeNoSkript   Emoji = "<:noskript:999194719005446265>"
+	CubeRider      Emoji = "<:rider:935740745778868235>"
+	CubeTypeScript Emoji = "<:typescript:935737663594586163>"
+	CubeVSC        Emoji = "<:vsc:935740848916820039>"
 )
