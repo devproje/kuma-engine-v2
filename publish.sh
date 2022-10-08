af600812ee8bc4008c8a9ffb2a9cf89ae4d58a1e
Project_TL <me@projecttl.net>
2022-10-08 17:47:53 +0900

    fix: publisher

--- publish.sh
+++ publish.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-MODULE=github.com/devproje/kuma-engine@$(TAG)
+MODULE=github.com/devproje/kuma-engine@$1
 PROXY_URL=proxy.golang.org
 
 if [ "$1" == "default" ]; then
 	echo "Please type some tag"
 else
-	git tag $(TAG)
-	git push origin $(TAG)
+	git tag $1
+	git push origin $1
 	GOPROXY=$PROXY_URL go list -m $MODULE
 fi
